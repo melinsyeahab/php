4cadcd3ed5157cf6e53458c5576ae73f572c265f
kevinrodrigo997 <70495394+kevinrodrigo997@users.noreply.github.com>
2020-10-17 20:06:26 +0800

    Add files via upload

---
+++ admin/controller/c_login.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['email'])){
+    $conn=$con->koneksi();
+    $email=$_POST['email'];$psw=md5($_POST['password']);
+    $sql = "SELECT * FROM data_login where password ='$psw' and email ='$email' and active='Y'";
+    $user = $conn->query($sql);
+    if($user->num_rows > 0){
+        $sess=$user->fetch_array();
+        $_SESSION['login']['email']=$sess['email'];
+        $_SESSION['login']['id']=$sess['id'];
+        $url="http://localhost/php";
+        header('Location: http://localhost/php/admin/index.php?mod=dokter');
+    }else{
+        $msg="Email dan Password tidak cocok";
+        include_once 'views/v_login.php';
+        }
+}else{
+        include_once 'views/v_login.php';
+}
+?>
