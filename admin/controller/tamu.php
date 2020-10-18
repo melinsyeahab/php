4bc2a8be1e77503a755dd95adf57957538e08ef1
kevinrodrigo997 <70495394+kevinrodrigo997@users.noreply.github.com>
2020-10-18 16:11:09 +0800

    Create tamu.php

---
+++ admin/controller/tamu.php
@@ -0,0 +1,55 @@
+<?php
+$con->auth();
+$conn=$con->koneksi();
+switch (@$_GET['page']){
+    case 'add':
+        $content="views/tamu/tambah.php";
+    include_once "views/template.php";
+    break;
+    case 'save':
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            if(empty($_POST['nama_tamu'])){
+                $err['nama_tamu']="Wajib diisi";
+            }
+            if(empty($_POST['nik'])){
+                $err['nik']="wajib diisi";
+            }
+            if(empty($_POST['jenis_kelamin'])){
+                $err['jenis_kelamin']="wajib diisi";
+            }
+            if(empty($_POST['alamat'])){
+                $err['alamat']="wajib diisi";
+            }
+            if(!isset($err)){
+            $sql = "INSERT INTO tamu (nama_tamu, nik, jenis_kelamin, alamat) 
+            VALUES ('$_POST[nama_tamu]', '$_POST[nik]', '$_POST[jenis_kelamin]', '$_POST[alamat]')";
+            if ($conn->query($sql) === TRUE) {
+                header('Location: http://localhost:8080/php/admin/?mode=tamu');
+            }else {
+                $err['msg']= "Error: " . $sql . "<br>" . $conn->error;
+            }}
+        }else{
+            $err['msg']="tidak diijinkan";
+        }
+    break;
+    case 'edit':
+        $tamu ="select * from tamu where md5(nik)='$_GET[id]'";
+        $tamu=$conn->query($tamu);
+        $_POST=$tamu->fetch_assoc();
+        //var_dump($tamu);
+        $content="views/tamu/tambah.php";
+        include_once 'views/template.php';
+    break;
+    case 'delete';
+        $tamuk ="delete from tamu where md5(nik)='$_GET[id]'";
+        $tamu=$conn->query($tamu);
+        header('Location: http://localhost:8080/php/admin/?mode=tamu');
+    break;
+    default:
+    $sql = "select * from tamu";
+    $tamu=$conn->query($sql);
+    $conn->close();
+    $content="views/tamu/tampil.php";
+    include_once "views/template.php";
+}
+?>
